619bbcff9380fc9f6783945aba2843c626c90c08
Roberto Gianotto <gianottoroberto@gmail.com>
2021-04-04 09:13:50 +0200

    prima versione non rifattorizzata della registrazione di un nuovo utente in forma procedurale

---
+++ php_procedural/functions.php
@@ -0,0 +1,3 @@
+<?php
+
+// TODO: inserire qui dentro la rifattorizzazione delle funzioni di registrazione e login
--- php_procedural/register.php
+++ php_procedural/register.php
@@ -44,12 +44,58 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
 
+    ///////////////////////////////////////////////////////
+    /// Validazione del campo della password
+    ///////////////////////////////////////////////////////
 
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
 
+    ///////////////////////////////////////////////////////
+    /// Validazione del campo conferma password
+    ///////////////////////////////////////////////////////
 
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
 
+    /////////////////////////////////////////////////////////
+    /// Verifica di ulteriori errori di input prima dell'
+    /// inserimento nel database della registrazione
+    ///  del nuovo utente
+    /////////////////////////////////////////////////////////
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
 
+        // statement per l'inserimento a database del nuovo utente
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
 
+        if($stmt = mysqli_prepare($connection, $sql)){
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            // setting dei parametri per la query
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT);
+            // esecuzione dello statement query
+            if(mysqli_stmt_execute($stmt)){
+                // L'utente viene inserito e si viene ridirezionati alla pagina di Login
+                header("location: login.php");
+            } else{
+                echo "Oops! Qualcosa è andato storto. Riprova più tardi.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
     // Close connection
     mysqli_close($connection);
 }
@@ -67,27 +113,43 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <div class="wrapper">
     <h2>Sign Up</h2>
     <p>Compila questo form per creare un nuovo account.</p>
-    <form action="#" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
         <div class="form-group">
             <label>Username</label>
-            <input type="text" name="username" class="form-control" value="">
-            <span class="invalid-feedback"></span>
+            <input type="text" name="username"
+                   class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>"
+                   value="<?php echo $username; ?>"
+            >
+            <span class="invalid-feedback">
+                <?php echo $username_err; ?>
+            </span>
         </div>
         <div class="form-group">
             <label>Password</label>
-            <input type="password" name="password" class="form-control" value="">
-            <span class="invalid-feedback"></span>
+            <input type="password"
+                   name="password"
+                   class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>"
+                   value="<?php echo $password; ?>"
+            >
+            <span class="invalid-feedback">
+                <?php echo $password_err; ?>
+            </span>
         </div>
         <div class="form-group">
             <label>Confirm Password</label>
-            <input type="password" name="confirm_password" class="form-control" value="">
-            <span class="invalid-feedback"></span>
+            <input type="password" name="confirm_password"
+                   class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>"
+                   value="<?php echo $confirm_password; ?>"
+            >
+            <span class="invalid-feedback">
+                <?php echo $confirm_password_err; ?>
+            </span>
         </div>
         <div class="form-group">
             <input type="submit" class="btn btn-primary" value="Submit">
             <input type="reset" class="btn btn-secondary ml-2" value="Reset">
         </div>
-        <p>Already have an account? <a href="#">Login here</a>.</p>
+        <p>Already have an account? <a href="login.php">Login here</a>.</p>
     </form>
 </div>
 </body>
